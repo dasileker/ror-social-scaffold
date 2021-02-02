931f299b45bb87181670196b3bf52adbda3ebf09
dasileker <dasileker@gmail.com>
2021-02-02 09:48:22 +0100

    refactored the friendship controller + add friendship association for user.rb

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,36 +1,32 @@
 class FriendshipController < ApplicationController
-  def index
-    if user_signed_in?
-      @friendship = User.where('id<>?', friendid)
-      @friend_requests = current_user.friend_requests
-      @pending_friends = current_user.pending_friends
-      @friends = current_user.friends
-    else
-      redirect_to root_path
-    end
-  end
-
-  def new
-    @friendship = Friendship.new
-  end
-
   def create
-    @friendship = Friendship.create(friendship_params)
+    @friendship = current_user.friendships.build(friend_id: params[:user_id])
+    @friendship.status = false
+    @friendship.save
     redirect_to users_path
   end
 
-  def update
-    friendship = current_user.inverse_friendships.find_by(user_id: params[:friend_id])
-    friendship.status = true
-    if friendship.save
-      Friendship.create(user_id: current_user.id, friend_id: params[:friend_id], status: true)
-      redirect_to friendship_path
-    end
+  def accept
+    current_user.confirm_friend(User.find_by(id: params[:user_id]))
+    redirect_to users_path
+  end
+
+  def reject
+    current_user.reject_friend(User.find_by(id: params[:user_id]))
+    redirect_to users_path
+  end
+
+  def cancel
+    current_user.cancel_request(User.find_by(id: params[:user_id]))
+    redirect_to users_path
   end
 
   def destroy
-    friendship = current_user.inverse_friendships.find_by(user_id: params[:friend_id])
-    redirect_to friendship_path if friendship.destroy
+    f1 = Friendship.all.find_by(user_id: params[:user_id], friend_id: current_user.id)
+    f2 = Friendship.all.find_by(user_id: current_user.id, friend_id: params[:user_id])
+    f1&.destroy
+    f2&.destroy
+    redirect_to users_path
   end
 
   private
