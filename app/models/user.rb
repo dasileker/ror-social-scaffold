71d91963e65347728bb32dfb3fa62c3c1244c79a
dasileker <dasileker@gmail.com>
2021-02-02 10:04:10 +0100

    refactoring the friendship + user + helper rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,36 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   
+  has_many :pending_friendships, :class_name => 'Friendship', :foreign_key => 'user_id'
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+
+   has_many :friend_requests, through: :inverse_friendships, source: :user
+
+
+
+
+  def friends
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.status }
+    friends_array.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find_by(user_id: user.id)
+    friendship.status = true
+    friendship.save
+  end
+
+  def reject_friend(user)
+    friendship = inverse_friendships.find_by(user_id: user.id)
+    friendship.destroy
+  end
+
+  def cancel_request(user)
+    friendship = friendships.find_by(friend_id: user.id)
+    friendship.destroy
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
