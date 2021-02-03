9d63e3f2137094a4aa6ee16724739083b4691057
dasileker <dasileker@gmail.com>
2021-02-03 10:29:00 +0100

    implemented friendship with 2 rows

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
 
-  has_many :pending_friendships, -> { where status: false }, class_name: 'Friendship', foreign_key: 'user_id'
-  has_many :pending_friends, through: :pending_friendships, source: :friend
-
-  has_many :inverse_friendships, -> { where status: false }, class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :friend_requests, through: :inverse_friendships, source: :user
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.status }
