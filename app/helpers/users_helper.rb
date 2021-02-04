4318cf38ff3660e9cf5c8beeb9c7a753de623405
dasileker <dasileker@gmail.com>
2021-02-04 21:44:04 +0100

    the errors appearing on the github but not in the terminal you can check

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,8 @@
 module UsersHelper
   def button_display(user)
     if current_user.friend?(user)
-      val = link_to 'Remove Friend', friendship_path(id: current_user.id, user_id: user.id), method: :delete,
-                                                                                             class: 'user-button'
+      val = link_to 'Remove Friend', friendship_path(id: current_user.id, user_id: user.id),
+                    method: :delete, class: 'user-button'
       val += '  You are Friends'
     elsif current_user.pending_friends.include?(user)
       val = link_to 'Cancel Request', cancel_path(user_id: user.id), method: :post, class: 'user-button'
